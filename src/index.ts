71191eb2ca8a13864cd1038910dbcbcee5cb9f18
GenoBones <geno.bonello@gmail.com>
2022-10-09 18:38:25 -0700

    added some variables and included a type per variable

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
-import confetti from 'canvas-confetti';
+// import { v4 as uuidV4 } from 'uuid'
+// import confetti from 'canvas-confetti';
 
-console.log("Hi")
+// confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
+//   resize: true,
+//   useWorker: true,
+// })({ particleCount: 200, spread: 500 });
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 500 });
+// console.log(uuidV4())
+
+
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.querySelector<HTMLFormElement>("#new-task-form")
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
