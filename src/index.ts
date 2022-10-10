59eea3b96d91d1583c8905fe45af444a8c7eb9a4
GenoBones <geno.bonello@gmail.com>
2022-10-09 19:52:25 -0700

    created custom Type for form input

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,39 @@
-import confetti from 'canvas-confetti';
 import { v4 as uuidV4 } from "uuid"
+// import confetti from 'canvas-confetti';
+// document.addEventListener('click', function() {confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
+//   resize: true,
+//   useWorker: true,
+// })({ particleCount: 200, spread: 500 })})
 
+type Task = {
+  id: string
+  title: string
+  completed: boolean
+  cratedAt: Date
+}
 
 const list = document.querySelector<HTMLUListElement>("#list")
 const form = document.getElementById("#new-task-form") as HTMLFormElement | null
 const input = document.querySelector<HTMLInputElement>("#new-task-title")
 const button = document.getElementById("#add_button") as HTMLButtonElement
 
-document.addEventListener('click', function() {confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 500 })})
+
 
 form?.addEventListener("submit", e => {
   e.preventDefault()
 
   if (input?.value== "" || input?.value==null) return
 
-  const task = {
+  const newTask: Task = {
     id: uuidV4,
     title: input.value,
     complete: false,
     createdAt: new Date()
   }
 
-  input.value
+  addListItem(newTask)
 })
+
+function addListItem(task: Task) {
+  const item = document.createElement("li")
+}
